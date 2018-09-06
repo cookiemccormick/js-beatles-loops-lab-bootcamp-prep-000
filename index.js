7d5d67b391b0a27db97690904129692e76134325
Alisa McCormick <muntananuchat@gmail.com>
2018-09-06 21:32:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function johnLennonFacts(facts) {
   var array = []
   var i = 0
   while (facts.length > i){
-    array.push(`${facts[i]}`)
+    array.push(`${facts[i]}!!!`)
     i++
   }
   return array
